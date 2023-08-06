e4e352f77def7c8e93ca9033508ff713b0cf9be5
Batu <aleyhin1@gmail.com>
2023-08-06 15:41:49 +0300

    tileselection based on walkablepaths methods created

--- Assets/Scripts/TileScript.cs
+++ Assets/Scripts/TileScript.cs
@@ -1,5 +1,6 @@
     using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
@@ -9,10 +10,10 @@ public class TileScript : MonoBehaviour
     public Animator Animator;
     public ObjectList SelectedTiles;
     public SelectedFruit SelectedFruit;
+    public GameObject[] NeighbourTiles;
+    public bool IsWalkable = false;
 
-    private GameObject[] _neighbourTiles;
     private TileStateMachine _tileStateMachine;
-
     private Vector2[] _raycastVectors = { Vector2.left, Vector2.left + Vector2.up, Vector2.up, Vector2.up + Vector2.right
     , Vector2.right, Vector2.right + Vector2.down, Vector2.down, Vector2.down + Vector2.left};
 
@@ -24,15 +25,21 @@ public class TileScript : MonoBehaviour
     private void Start()
     {
         _tileStateMachine.Initialize(_tileStateMachine.IdleState);
+        SetNeighbourTiles();
     }
 
     private void OnMouseEnter()
     {
-        GetNeighbourTiles();
-        if (SelectedFruit.Type == string.Empty || gameObject.tag == SelectedFruit.Type)
+        if (SelectedFruit.Type == string.Empty)
         {
             _tileStateMachine.TransitionTo(_tileStateMachine.SelectedState);
             SelectedFruit.Type = gameObject.tag;
+            SetWalkableOnNeighbours();
+        }
+        else if (gameObject.tag == SelectedFruit.Type && IsWalkable)
+        {
+            _tileStateMachine.TransitionTo(_tileStateMachine.SelectedState);
+            SetWalkableOnNeighbours();
         }
     }
 
@@ -41,16 +48,26 @@ public class TileScript : MonoBehaviour
         _tileStateMachine.Update();
     }
 
-    private void GetNeighbourTiles()
+    private void SetNeighbourTiles()
     {
         foreach (Vector2 directionVectors in _raycastVectors)
         {
             RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, directionVectors, 1f);
             if (hitInfo != false)
             {
-                Debug.Log(hitInfo.collider.gameObject.tag);
+                NeighbourTiles = NeighbourTiles.Append(hitInfo.collider.gameObject).ToArray();
             }
         }
-        
+    }
+
+    private void SetWalkableOnNeighbours()
+    {
+        foreach (GameObject tile in NeighbourTiles)
+        {
+            if (tile.tag == SelectedFruit.Type)
+            {
+                tile.GetComponent<TileScript>().IsWalkable = true;
+            }
+        }
     }
 }
